33e96591b36dc3b38e702fdedaa2579952f30a80
Sooraj S Prakash <sooraj.prakash@runo.in>
2023-02-20 11:13:58 +0530

    intro in vue

--- 1. intro/app.js
+++ 1. intro/app.js
@@ -1,14 +1,33 @@
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app')
 
-const button = document.querySelector('button');
-const input = document.querySelector('input');
-const list = document.querySelector('ul');
 
-function addGoal() {
-    const goal = input.value;
-    const listItem = document.createElement('li');
-    listItem.textContent = goal;
-    list.appendChild(listItem);
-    input.value = '';
-}
 
-button.addEventListener('click', addGoal);
+
+
+
+// const button = document.querySelector('button');
+// const input = document.querySelector('input');
+// const list = document.querySelector('ul');
+
+// function addGoal() {
+//     const goal = input.value;
+//     const listItem = document.createElement('li');
+//     listItem.textContent = goal;
+//     list.appendChild(listItem);
+//     input.value = '';
+// }
+
+// button.addEventListener('click', addGoal);
