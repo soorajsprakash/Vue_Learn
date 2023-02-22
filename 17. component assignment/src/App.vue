7881825d18547111dbcc82135c27221b416aec30
Sooraj S Prakash <soorajsprakash007@gmail.com>
2023-02-22 18:44:58 +0530

    component, props & custom event assessment

---
+++ 17. component assignment/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <h1>Component assessment</h1>
+    <active-user
+        :users="users"
+    ></active-user>
+    <user-data
+        @add-user="AddUser"
+    ></user-data>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            users: [
+                { 
+                    username: "Sun",
+                    userage: 22
+                },
+                {
+                    username: "Moon",
+                    userage: 29
+                }
+            ]
+        }
+    },
+    methods: {
+        AddUser(name, age) {
+            this.users.push({
+                username: name,
+                userage: +age // this converts string to number
+            })
+        }
+    }
+}
+</script>
+
+<style>
+section.add-user {
+    background-color: burlywood;
+    border: 50px
+}
+section.active-users {
+    background-color: cyan;
+    border: 50px
+}
+</style>
---
+++ 17. component assignment/src/components/UserData.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="add-user">
+        <h2>Add User</h2>
+        <form @submit.prevent="submitUser">
+            <div>
+                <label for="name">Name</label>
+                <input type="text" id="name" v-model="enteredName">
+            </div>
+            <div>
+                <label for="age">Age</label>
+                <input type="text" id="age" v-model="enteredAge">
+            </div>
+            <button>Add User</button>
+        </form>
+    </section>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            enteredName: '',
+            enteredAge: ''
+        }
+    },
+    emits: ['add-user'],
+    methods: {
+        submitUser() {
+            this.$emit('add-user', this.enteredName, this.enteredAge)
+            this.enteredName = this.enteredAge = ''
+        }
+    }
+}
+</script>
