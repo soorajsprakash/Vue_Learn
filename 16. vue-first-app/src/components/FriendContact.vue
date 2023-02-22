3e4afe96674e2092b3d82b8a183c4a2ab3474b7e
Sooraj S Prakash <sooraj.prakash@runo.in>
2023-02-22 13:06:42 +0530

    intro to cli + first vue app

---
+++ 16. vue-first-app/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ detailsVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detailsVisible">
+            <li><strong>Phone: </strong>{{ friend.phone }}</li>
+            <li><strong>Email: </strong>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+
+
+<script>
+export default {
+    data() {
+        return {
+            detailsVisible: false,
+            friend: {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '5411321336',
+                email: 'manuel@gmail.com'
+            },
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsVisible = !this.detailsVisible;
+        }
+    }
+};
+</script>
