fdd4f6da87ba2f4aa5bfc333d921d2f4d6a5936f
Sooraj S Prakash <sooraj.prakash@runo.in>
2023-02-22 16:25:27 +0530

    new component => add friend

--- 16. vue-first-app/src/App.vue
+++ 16. vue-first-app/src/App.vue
@@ -3,6 +3,7 @@
         <header>
             <h1>My Friends</h1>
         </header>
+        <add-friend @add-contact="addContact"></add-friend>
         <ul>
             <friend-contact 
                 v-for="friend in friends"
@@ -45,6 +46,15 @@ export default {
         toggleFavStatus(friendId) {
             const friend = this.friends.find((f) => f.id === friendId)
             friend.isFavorite = !friend.isFavorite
+        },
+        addContact(name, phone, email) {
+            const newFriend = {
+                id: new Date().toISOString(),
+                name,
+                phone,
+                email,
+            }
+            this.friends.push(newFriend)
         }
     }
 };
@@ -83,7 +93,7 @@ header {
     list-style: none;
 }
 
-#app li {
+#app li, #app form {
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
     margin: 1rem auto;
     border-radius: 10px;
@@ -116,4 +126,19 @@ header {
     border-color: #ec3169;
     box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
 }
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
+}
+
 </style>
