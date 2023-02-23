1ace9084d15fe4ba562f19e10822c755c4556f0e
Sooraj S Prakash <soorajsprakash007@gmail.com>
2023-02-23 12:51:47 +0530

    scoped slot

--- 19. advanced comp comm/src/App.vue
+++ 19. advanced comp comm/src/App.vue
@@ -7,6 +7,16 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <!-- "#default" is the equivalent for "v-slot:default" -->
+      <!-- here we don't really need to use the template tag, instead we can include the slot ref inside the <course-goals> -->
+      <!-- for eg:  <course-goals #default="slotItem">...</course-goals> -->
+      <template #default="slotItem">
+        <!-- here the slotItem would be an object with each item passed from the source -->
+        <h2>{{ slotItem.item }}</h2>
+        <p>{{ slotItem["anotherProp"] }}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -14,13 +24,15 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue'
 
 export default {
   components: {
     'the-header': TheHeader,
     // by doing in the below method, we can use self closing tags like "<BadgeList />
     BadgeList: BadgeList,
-    UserInfo
+    UserInfo,
+    CourseGoals
   },
   data() {
     return {
---
+++ 19. advanced comp comm/src/components/CourseGoals.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+            <!-- passing data from slot to where we use slot -->
+            <slot :item="goal" anotherProp="..."></slot>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            goals: ['finish course', 'learn vue']
+        }
+    }
+}
+</script>
