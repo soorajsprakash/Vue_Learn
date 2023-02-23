876727f8eec6d9d7c93c7f8ab2782f6c9f01a8c8
Sooraj S Prakash <soorajsprakash007@gmail.com>
2023-02-23 16:48:30 +0530

    first component & props

---
+++ 20. PROJECT RememberMe/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <ul>
+        <LearningResource 
+            v-for="resource in storedResources"
+            :key="resource.id"
+            :title="resource.title"
+            :description="resource.description"
+            :link="resource.link"
+        ></LearningResource>
+    </ul>
+</template>
+
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue'
+
+
+export default {
+    components: {
+        LearningResource,
+    },
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'Official vuejs documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'googl',
+                    title: 'Google',
+                    description: 'Google search engine',
+                    link: 'https://google.com'
+                },
+            ]
+        }
+    }
+}
+</script>
+
