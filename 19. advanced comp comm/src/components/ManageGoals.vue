1fdbcee1c4f3649884e0c802f1920d9746a559e5
Sooraj S Prakash <soorajsprakash007@gmail.com>
2023-02-23 15:04:14 +0530

    custom overlay/dialog box

--- 19. advanced comp comm/src/components/ManageGoals.vue
+++ 19. advanced comp comm/src/components/ManageGoals.vue
@@ -1,6 +1,40 @@
 <template>
     <div>
         <h2>Manage Goals</h2>
-        <input type="text" name="" id="">
+        <input type="text" ref="goal">
+        <button @click="setGoal">Set Goal</button>
+        <error-alert v-if="inputIsInvalid">
+            <h2>Input is invalid</h2>
+            <p>Pls enter at least a few chars</p>
+            <button @click="closeError">Okay</button>
+        </error-alert>
     </div>
 </template>
+
+
+<script>
+import ErrorAlert from './ErrorAlert.vue'
+
+export default {
+    components: {
+        ErrorAlert
+    },
+    data() {
+        return {
+            inputIsInvalid: false
+        }
+    },
+     methods: {
+        setGoal() {
+            const enteredValue = this.$refs.goal.value;
+            if (enteredValue === '') {
+                // alert('input must not be empty')
+                this.inputIsInvalid = true
+            }
+        },
+        closeError() {
+            this.inputIsInvalid = false;
+        }
+     }
+}
+</script>
