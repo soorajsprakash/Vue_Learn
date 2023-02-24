9ec13337f6929fcfc9387dd24bcee93d548c73d8
Sooraj S Prakash <soorajsprakash007@gmail.com>
2023-02-24 14:16:44 +0530

    handling browser side error

--- 22. http req/src/components/survey/UserExperiences.vue
+++ 22. http req/src/components/survey/UserExperiences.vue
@@ -6,8 +6,12 @@
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">
+        {{ error }}
+      </p>
       <p v-else-if="!isLoading && (!results || results.length === 0)">No stored exp found.</p>
-      <ul v-if="!isLoading && results && results.length > 0">
+      
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -29,12 +33,14 @@ export default {
   data() {
     return {
       results: [],
-      isLoading: false
+      isLoading: false,
+      error: null
     }
   },
   methods: {
     loadExperiences() {
       this.isLoading = true
+      this.error = null
       fetch('https://vue-http-demo-6d22f-default-rtdb.firebaseio.com/surveys.json')
         .then((res) => {
           if (res.ok) {
@@ -53,6 +59,11 @@ export default {
             })
             this.results = results
           }
+        })
+        .catch((e) => {
+          console.log(e)
+          this.isLoading = false
+          this.error = "Failed to fetch data, please try again!"
         })
     }
   },
