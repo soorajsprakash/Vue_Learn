8279de1d674a91734c3c94e746c8dbbec31488e6
Sooraj S Prakash <soorajsprakash007@gmail.com>
2023-02-27 11:55:38 +0530

    passing data with route params

--- 23. routing/src/main.js
+++ 23. routing/src/main.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue'
+import TeamMembers from './components/teams/TeamMembers.vue'
 import UsersList from './components/users/UsersList.vue'
 
 
@@ -16,6 +17,10 @@ const router = createRouter({
         {
             path: '/users',
             component: UsersList
+        },
+        {
+            path: '/teams/:teamId',
+            component: TeamMembers
         },
     ],
     linkActiveClass: 'active',
