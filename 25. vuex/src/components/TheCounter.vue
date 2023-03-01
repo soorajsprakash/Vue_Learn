532a8eda062bf1ad08ac2567224051255de834f9
Sooraj S Prakash <soorajsprakash007@gmail.com>
2023-03-01 11:55:49 +0530

    vuex intro - global state

---
+++ 25. vuex/src/components/TheCounter.vue
@@ -0,0 +1,16 @@
+<template>
+    <h3>{{ counter }}</h3>
+</template>
+
+
+<script>
+
+export default {
+  
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+  },
+};
+</script>
