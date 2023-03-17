067e7d2a0143f15d3c33bc6c1cf3fec19888e1ee
Sooraj S Prakash <soorajsprakash007@gmail.com>
2023-03-17 22:14:24 +0530

    Component communication --> Practise

---
+++ 17. Practise comp comm/src/components/ActiveUser.vue
@@ -0,0 +1,21 @@
+<template>
+    <section  class="active-users">
+        <h2>Active Users</h2>
+        <div v-for="user in this.users" :key="user">
+            <h3>{{ user.username }}</h3>
+            <h4>{{ user.userage }} yrs</h4>
+        </div>
+    </section>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: ['users']
+}
+</script>
